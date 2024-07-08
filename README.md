506bf9a40e5d9ac1d1d167db544bc3e9bc171361
hamataki <hamatakiryunosuke@gmail.com>
2024-07-08 10:00:54 +0900

    insert.phpでDBに保存、写真のアップロードに初挑戦

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # cheerpark_ver4
+①課題番号-プロダクト名
+cheerpark_ver4
+
+②課題内容（どんな作品か）
+ログイン機能＋
+
+③DEMO
+デプロイしている場合はURLを記入（任意） https://cheerpark.sakura.ne.jp/cheerpark_ver4/index.php
+
+④作ったアプリケーション用のIDまたはPasswordがある場合
+ID: 〇〇〇〇〇〇〇〇
+PW: 〇〇〇〇〇〇〇〇
+
+⑤工夫した点・こだわった点
+
+1.紙に全体像を書き出す
+
+2.Login.php→home.php→list.phpのhtmlだけ作成
+
+3.画面遷移を追加
+
+4.funcs.phpでDB接続関数とその他関数を作成
+ 
+5.insert.phpでDBに保存、今回は写真のアップロードに初挑戦
+[参照]　画像保存：https://zenn.dev/zennzenn_ok/articles/b910505b811cd7
+
+6.list.phpに写真を表示
+
+以下、やりたかったこと
+7.
+8.
+9.
+
+
+⑦質問・疑問・感想、シェアしたいこと等なんでも
+[感想]　
+[参照]　画像保存：https://zenn.dev/zennzenn_ok/articles/b910505b811cd7,
+ファイルアップロードおまじない：https://mugenup-tech.hatenadiary.com/entry/2014/08/28/100910,
---
+++ insert.php
@@ -0,0 +1,43 @@
+<?php
+require_once('funcs.php');
+
+// POSTデータ取得
+$imgfile = $_FILES['imgfile'];
+$note = $_POST['note'];
+$genre = $_POST['genre'];
+
+// DB接続します
+$pdo = db_conn();
+
+// ファイルが送信されているか、エラーがないかをチェック
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+} else {
+    if (isset($_FILES['imgfile']) && $_FILES['imgfile']['error'] === UPLOAD_ERR_OK) {
+        
+        //['gazou']['tmp_name']は画像のパス
+        //file_get_contents() は、指定したファイルの内容全体を文字列として読み込むPHP関数
+        $content = file_get_contents($_FILES['imgfile']['tmp_name']);
+
+        //データ登録SQL作成
+        $stmt = $pdo->prepare('INSERT INTO
+        cheerpark_an_table4( id, imgfile, note, genre, date )
+        VALUES( NULL, :imgfile, :note, :genre, now() ) ');
+
+        // バインド変数を用意
+        // 画像データをバイナリ形式でデータベースに挿入
+        $stmt->bindValue(':imgfile', $content, PDO::PARAM_LOB);
+        $stmt->bindValue(':note', $note, PDO::PARAM_STR);
+        $stmt->bindValue(':genre', $genre, PDO::PARAM_STR);
+
+        // 実行
+        $status = $stmt->execute(); //true or false
+
+        // データ登録処理後
+        if ($status === false) {
+            sql_error($stmt);
+        } else {
+            //select.phpへリダイレクト
+            redirect('list.php');
+        };
+    };
+};
